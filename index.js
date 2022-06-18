21f5fe6eaccb5b74bd19f7f4df8a68354b44a069
Rhythm Bansal <rskbansal@outlook.com>
2022-06-18 05:35:40 +0530

    intial commit

--- index.js
+++ index.js
@@ -6,10 +6,17 @@ document.querySelector('.search_input').addEventListener('keyup', e => {
     switch(search_engine){
         case 'google':
         case 'youtube':
+        case 'slides':
+        case 'sheets':
+        case 'drive':
         case 'docs': break;
         default: search_engine = 'google';
-        search_query = input;
+                 search_query = 'google ' + input; // adding google if no search engine specified
     }
+    console.log(search_query);
+
+    let find=search_query.split(' ').slice(1).join(' '); // fixed search query
+
     if(search_engine !== current_search) {
         current_search = search_engine;
         const image = document.querySelector('.search_icon');
@@ -26,17 +33,38 @@ document.querySelector('.search_input').addEventListener('keyup', e => {
                 image.src = 'docs.png';
                 image.alt = 'Google Docs icon';
                 break;
+            case 'slides':
+                image.src = 'slides.png';
+                image.alt = 'Google Slides icon';
+                break;
+            case 'sheets':
+                image.src = 'sheets.png';
+                image.alt = 'Google Sheets icon';
+                break;
+            case 'drive':
+                image.src = 'drive.png';
+                image.alt = 'Google Drive icon';
+                break;
         }
     }
     if(e.key === 'Enter')
         switch(search_engine) {
             case 'docs':
-                window.location = `https://docs.google.com/document/?q=${search_query}`;
+                window.location = `https://docs.google.com/document/?q=${find}`;
                 break;
             case 'youtube': 
-                window.location = `https://www.youtube.com/results?search_query=${search_query}`;
+                window.location = `https://www.youtube.com/results?search_query=${find}`;
                 break;
+            case 'slides': 
+            window.location = `https://docs.google.com/presentation/?q=${find}`;
+            break;
+            case 'sheets': 
+            window.location = `https://docs.google.com/spreadsheets/?q=${find}`;
+            break;
+            case 'drive': 
+            window.location = `https://drive.google.com/drive/search?q=${find}`;
+            break;
             default: 
-                window.location = `https://www.google.com/search?q=${search_query}`;
+                window.location = `https://www.google.com/search?q=${find}`;
         }
 });
