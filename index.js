4517f4a380fcd4c73ccfaefc3bb5e9d4c22c46a9
yatharth(@slayingmaverick) <yatharthnanda25@gmail.com>
2022-06-19 22:43:23 +0530

    adding changes

--- index.js
+++ index.js
@@ -1,42 +1,50 @@
-let current_search = 'google'
-document.querySelector('.search_input').addEventListener('keyup', e => {
-    const input = e.target.value;
-    let search_engine = input.split(' ')[0].toLowerCase();
-    let search_query = input.split(' ').splice(0).join(' ');
-    switch(search_engine){
-        case 'google':
-        case 'youtube':
-        case 'docs': break;
-        default: search_engine = 'google';
-        search_query = input;
+let current_search = "google";
+document.querySelector(".search_input").addEventListener("keyup", (e) => {
+  const input = e.target.value;
+  let search_engine = input.split(" ")[0].toLowerCase();
+  console.log("the search engine is \n");
+  console.log(search_engine);
+  let search_query = input.split(" ");
+  search_query.shift(); //didnt know how to integrate into a compound command so i removed the first element separately and then reformed the string
+  search_query = search_query.splice(0).join(" ");
+  console.log("the search query is \n");
+  console.log(search_query);
+  switch (search_engine) {
+    case "google":
+    case "youtube":
+    case "docs":
+      break;
+    default:
+      search_engine = "google";
+      search_query = input;
+  }
+  if (search_engine !== current_search) {
+    current_search = search_engine;
+    const image = document.querySelector(".search_icon");
+    switch (search_engine) {
+      case "google":
+        image.src = "google.png";
+        image.alt = "Google icon";
+        break;
+      case "youtube":
+        image.src = "youtube.png";
+        image.alt = "YouTube icon";
+        break;
+      case "docs":
+        image.src = "docs.png";
+        image.alt = "Google Docs icon";
+        break;
     }
-    if(search_engine !== current_search) {
-        current_search = search_engine;
-        const image = document.querySelector('.search_icon');
-        switch(search_engine){
-            case 'google':
-                image.src = 'google.png';
-                image.alt = 'Google icon';
-                break;
-            case 'youtube':
-                image.src = 'youtube.png';
-                image.alt = 'YouTube icon';
-                break;
-            case 'docs':
-                image.src = 'docs.png';
-                image.alt = 'Google Docs icon';
-                break;
-        }
+  }
+  if (e.key === "Enter")
+    switch (search_engine) {
+      case "docs":
+        window.location = `https://docs.google.com/document/?q=${search_query}`;
+        break;
+      case "youtube":
+        window.location = `https://www.youtube.com/results?search_query=${search_query}`;
+        break;
+      default:
+        window.location = `https://www.google.com/search?q=${search_query}`;
     }
-    if(e.key === 'Enter')
-        switch(search_engine) {
-            case 'docs':
-                window.location = `https://docs.google.com/document/?q=${search_query}`;
-                break;
-            case 'youtube': 
-                window.location = `https://www.youtube.com/results?search_query=${search_query}`;
-                break;
-            default: 
-                window.location = `https://www.google.com/search?q=${search_query}`;
-        }
 });
