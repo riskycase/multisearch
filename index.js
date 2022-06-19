92655fb24b0f7e8f3ae5f0658f2679bfec6acf09
srikar <f20212929@goa.bits-pilani.ac.in>
2022-06-19 23:23:56 +0530

    srikarkolukula_multisearch

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ document.querySelector('.search_input').addEventListener('keyup', e => {
     const input = e.target.value;
     let search_engine = input.split(' ')[0].toLowerCase();
     let search_query = input.split(' ').splice(0).join(' ');
+    var search_query1;
     switch(search_engine){
         case 'google':
         case 'youtube':
-        case 'docs': break;
+        case 'docs':
+        case 'drive':
+        case 'slides':
+        case 'sheets': break;
         default: search_engine = 'google';
         search_query = input;
     }
@@ -26,15 +30,42 @@ document.querySelector('.search_input').addEventListener('keyup', e => {
                 image.src = 'docs.png';
                 image.alt = 'Google Docs icon';
                 break;
+            case 'drive':
+                image.src = 'drive.png';
+                image.alt = 'Google Drive icon';
+                break;
+            case 'slides':
+                image.src = 'slides.png';
+                image.alt = 'Google Slides icon';
+                break;
+            case 'sheets':
+                image.src = 'sheets.png';
+                image.alt = 'Google Sheets icon';
+                break;
         }
     }
+    
     if(e.key === 'Enter')
         switch(search_engine) {
             case 'docs':
-                window.location = `https://docs.google.com/document/?q=${search_query}`;
+                search_query1 = search_query.slice(5);
+                window.location = `https://docs.google.com/document/?q=${search_query1}`;
                 break;
             case 'youtube': 
-                window.location = `https://www.youtube.com/results?search_query=${search_query}`;
+            search_query1 = search_query.slice(8);
+                window.location = `https://www.youtube.com/results?search_query=${search_query1}`;
+                break;
+            case 'drive':
+                search_query1 = search_query.slice(6);
+                window.location = `https://drive.google.com/drive/search?q=${search_query1}`;
+                break;
+            case 'slides':
+                search_query1 = search_query.slice(7);
+                window.location = `https://docs.google.com/presentation/u/0/?tgif=d&q=${search_query1}`;
+                break;
+            case 'sheets':
+                search_query1 = search_query.slice(7);
+                window.location = `https://docs.google.com/spreadsheets/u/0/?tgif=d&q=${search_query1}`;
                 break;
             default: 
                 window.location = `https://www.google.com/search?q=${search_query}`;
