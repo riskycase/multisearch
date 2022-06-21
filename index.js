d0e8ca8e6239611b0fdabb24f8318688a50cf25c
Ayush <ayush2003malik@gmail.com>
2022-06-21 20:16:26 +0530

    Add support for Google Drive, Google Slides and Google Sheets.

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ document.querySelector('.search_input').addEventListener('keyup', e => {
     let search_query = input.split(' ').splice(1).join(' ');
     switch(search_engine){
         case 'google':
+        case 'drive':
+        case 'slides':
+        case 'sheets':
         case 'youtube':
         case 'docs': break;
         default: search_engine = 'google';
@@ -26,6 +29,19 @@ document.querySelector('.search_input').addEventListener('keyup', e => {
                 image.src = 'docs.png';
                 image.alt = 'Google Docs icon';
                 break;
+            case 'drive':
+                image.src = 'drive.png';
+                image.alt = 'Google Drive icon';
+                break;
+            case 'slides':
+                image.src = 'slides.png';
+                image.alt = 'Google slides icon';
+                break;
+            case 'sheets':
+                image.src = 'sheets.png';
+                image.alt = 'Google sheets icon';
+                break;
+
         }
     }
     if(e.key === 'Enter')
@@ -35,6 +51,15 @@ document.querySelector('.search_input').addEventListener('keyup', e => {
                 break;
             case 'youtube': 
                 window.location = `https://www.youtube.com/results?search_query=${search_query}`;
+                break;
+            case 'drive':
+                window.location = `https://drive.google.com/drive/search?q=${search_query}` ;
+                break;
+            case 'slides':
+                window.location = `https://docs.google.com/presentation/u/0/?tgif=d&q=${search_query}` ;
+            break;
+            case 'sheets':
+                window.location = `https://docs.google.com/spreadsheets/u/0/?tgif=d&q=${search_query}` ;
                 break;
             default: 
                 window.location = `https://www.google.com/search?q=${search_query}`;
