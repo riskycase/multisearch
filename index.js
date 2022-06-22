20295025ff63be521c2657160248aae66e65dcd5
aaronrockmenezes <aaronrockmenezes@gmail.com>
2022-06-22 11:28:51 +0530

    Added Functionality for Google Drive, Slides and Sheets and Spotify

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ let current_search = 'google'
 document.querySelector('.search_input').addEventListener('keyup', e => {
     const input = e.target.value;
     let search_engine = input.split(' ')[0].toLowerCase();
-    let search_query = input.split(' ').splice(0).join(' ');
+    let search_query = input.split(' ').splice(1).join(' ');
     switch(search_engine){
         case 'google':
         case 'youtube':
-        case 'docs': break;
+        case 'docs': 
+        case 'sheets':
+        case 'drive':
+        case 'slides':
+        case 'spotify': 
+            break;
         default: search_engine = 'google';
         search_query = input;
     }
@@ -26,6 +31,22 @@ document.querySelector('.search_input').addEventListener('keyup', e => {
                 image.src = 'docs.png';
                 image.alt = 'Google Docs icon';
                 break;
+            case 'sheets':
+                image.src = 'google-sheets.png';
+                image.alt = 'Google sheets icon';
+                break;
+            case 'drive':
+                image.src = 'google-drive.png';
+                image.alt = 'Google Drive icon';
+                break;
+            case 'slides':
+                image.src = 'google-slides.png';
+                image.alt = 'Google Slides icon';
+                break;
+            case 'spotify':
+                image.src = 'spotify.png';
+                image.alt = 'Spotify icon';
+                break;
         }
     }
     if(e.key === 'Enter')
@@ -35,6 +56,18 @@ document.querySelector('.search_input').addEventListener('keyup', e => {
                 break;
             case 'youtube': 
                 window.location = `https://www.youtube.com/results?search_query=${search_query}`;
+                break;
+            case 'sheets': 
+                window.location = `https://docs.google.com/spreadsheets/?q=${search_query}`;
+                break;
+            case 'drive': 
+                window.location = `https://drive.google.com/drive/search?q=${search_query}`;
+                break;
+            case 'slides': 
+                window.location = `https://docs.google.com/presentation/?q=${search_query}`;
+                break;
+            case 'spotify': 
+                window.location = `https://open.spotify.com/search/${search_query}`;
                 break;
             default: 
                 window.location = `https://www.google.com/search?q=${search_query}`;
