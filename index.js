ed0bdecc68baaf23e26355a28319203caa8b53d7
diyar03 <f20212693@goa.bits-pilani.ac.in>
2022-06-22 12:12:56 +0530

    4 out of 5 tasks done

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ document.querySelector('.search_input').addEventListener('keyup', e => {
     switch(search_engine){
         case 'google':
         case 'youtube':
-        case 'docs': break;
+        case 'docs':
+        case 'slides':
+        case 'drive':
+        case 'sheets':
+        case 'soundcloud':
+ break;
         default: search_engine = 'google';
         search_query = input;
     }
@@ -26,6 +31,21 @@ document.querySelector('.search_input').addEventListener('keyup', e => {
                 image.src = 'docs.png';
                 image.alt = 'Google Docs icon';
                 break;
+            case 'slides':
+                image.src = 'slides.png';
+                image.alt='Google Slides icon';
+                break;
+            case 'drive':
+                image.src = 'drive.png';
+                image.alt ='Google Drive icon';
+                break;
+            case 'sheets':
+                image.src='sheets.png';
+                image.alt='Google Sheets icon';
+            case 'soundcloud':
+                image.src='soundcloud.png';
+                image.alt = 'Soundcloud icon';
+                break;
         }
     }
     if(e.key === 'Enter')
@@ -36,7 +56,19 @@ document.querySelector('.search_input').addEventListener('keyup', e => {
             case 'youtube': 
                 window.location = `https://www.youtube.com/results?search_query=${search_query}`;
                 break;
+            case 'sheets':
+                window.location = `https://sheets.google.com/spreadsheet/?q=${search_query}`;
+                break;
+            case 'drive':
+                window.location =`https://drive.google.com/drive/?q=${search_query}`;
+                break;
+            case 'slides':
+                window.location =`https://slides.google.com/slides/?q=${search_query}`;
+                break;
+            case 'soundcloud':
+                window.location=`https://soundcloud.com/?q=${search_query}`;
             default: 
                 window.location = `https://www.google.com/search?q=${search_query}`;
+
         }
 });
