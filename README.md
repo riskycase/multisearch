2b466ffdde640bb0e9d32d21a50cc61d45f532b7
pavan-bhat17 <f20212520@goa.bits-pilani.ac.in>
2022-06-22 16:48:30 +0530

    Final website

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ Vanilla Web implementation of a single site to search from multiple providers
 
 Supported providers (first_word):
 - Google (google) (Default)
+- Google Drive (drive)
+- Google Slides (slides)
+- Google Sheets (sheets)
+- Spotify (spotify)
 - YouTube (youtube)
 - Google Docs (docs)
 
--- index.js
+++ index.js
@@ -5,11 +5,17 @@ document.querySelector('.search_input').addEventListener('keyup', e => {
     let search_query = input.split(' ').splice(0).join(' ');
     switch(search_engine){
         case 'google':
+        case 'drive':
+        case 'slides':
+        case 'sheets':
+        case 'spotify':
         case 'youtube':
         case 'docs': break;
         default: search_engine = 'google';
         search_query = input;
     }
+    let query = search_query.replace((search_engine + ' '), '');
+    // console.log(query);
     if(search_engine !== current_search) {
         current_search = search_engine;
         const image = document.querySelector('.search_icon');
@@ -17,6 +23,22 @@ document.querySelector('.search_input').addEventListener('keyup', e => {
             case 'google':
                 image.src = 'google.png';
                 image.alt = 'Google icon';
+                break;
+            case 'drive':
+                image.src = 'drive.png';
+                image.alt = 'Google Drive icon';
+                break;
+            case 'slides':
+                image.src = 'slides.png';
+                image.alt = 'Google Slides icon';
+                break;
+            case 'sheets':
+                image.src = 'sheets.png';
+                image.alt = 'Google Sheets icon';
+                break;
+            case 'spotify':
+                image.src = 'spotify.png';
+                image.alt = 'Spotify icon';
                 break;
             case 'youtube':
                 image.src = 'youtube.png';
@@ -31,12 +53,24 @@ document.querySelector('.search_input').addEventListener('keyup', e => {
     if(e.key === 'Enter')
         switch(search_engine) {
             case 'docs':
-                window.location = `https://docs.google.com/document/?q=${search_query}`;
+                window.location = `https://docs.google.com/document/?q=${query}`;
                 break;
-            case 'youtube': 
-                window.location = `https://www.youtube.com/results?search_query=${search_query}`;
+            case 'drive':
+                window.location = `https://drive.google.com/drive/u/0/search?q=${query}`;
                 break;
-            default: 
-                window.location = `https://www.google.com/search?q=${search_query}`;
+            case 'slides':
+                window.location = `https://docs.google.com/presentation/?q=${query}`;
+                break;
+            case 'sheets':
+                window.location = `https://docs.google.com/spreadsheets/?q=${query}`;
+                break;
+            case 'spotify':
+                window.location = `https://open.spotify.com/search/${query}`;
+                break;
+            case 'youtube':
+                window.location = `https://www.youtube.com/results?search_query=${query}`;
+                break;
+            default:
+                window.location = `https://www.google.com/search?q=${query}`;
         }
 });
