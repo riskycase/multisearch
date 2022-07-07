91728763c312b19d9d6479a10fe887d77473e40e
hrishav77 <f20210629@goa.bits-pilani.ac.in>
2022-07-07 22:48:33 +0530

    added the drive,slides,sheets png, added support for Google Drive, Slides and Sheets and centered element

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ let current_search = 'google'
 document.querySelector('.search_input').addEventListener('keyup', e => {
     const input = e.target.value;
     let search_engine = input.split(' ')[0].toLowerCase();
-    let search_query = input.split(' ').splice(0).join(' ');
+    let search_query = input.split(' ').splice(1).join(' ');
     switch(search_engine){
         case 'google':
         case 'youtube':
-        case 'docs': break;
+        case 'docs':
+        case 'drive': 
+        case 'slides':
+        case 'sheets': break;
         default: search_engine = 'google';
         search_query = input;
     }
@@ -26,6 +29,19 @@ document.querySelector('.search_input').addEventListener('keyup', e => {
                 image.src = 'docs.png';
                 image.alt = 'Google Docs icon';
                 break;
+            case 'drive':
+                image.src = 'drive.png';
+                image.alt = 'Google drive icon';
+                break;
+            case 'slides':
+                image.src = 'slides.png';
+                image.alt = 'Google slides icon';
+                break;
+            case 'sheets':
+                image.src = 'sheets.png';
+                image.alt = 'Google sheets icon';
+                break;
+
         }
     }
     if(e.key === 'Enter')
@@ -35,6 +51,15 @@ document.querySelector('.search_input').addEventListener('keyup', e => {
                 break;
             case 'youtube': 
                 window.location = `https://www.youtube.com/results?search_query=${search_query}`;
+                break;
+            case 'drive':
+                window.location =`https://drive.google.com/drive/search?q=${search_query}`;
+                break;
+            case 'slides':
+                window.location =`https://docs.google.com/presentation/u/0/?tgif=d&q=${search_query}`;
+                break;
+            case 'sheets':
+                window.location =`https://docs.google.com/spreadsheets/u/0/?tgif=d&q=${search_query}`;
                 break;
             default: 
                 window.location = `https://www.google.com/search?q=${search_query}`;
